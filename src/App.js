41efcf747d7c441f4d6fad57e426843b600331d9
akaAditya <mymail.adityapatel@gmail.com>
2023-09-07 21:49:54 +0530

    fourth commit used thunk and fetch data

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./redux-store/ui-store";
 import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./redux-store/cart-action-creator";
 
 let isLoaded = true;
 
@@ -15,51 +15,20 @@ function App() {
   const notification = useSelector((state)=>state.ui.showStatus)
 
   useEffect(() => {
-    const cartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        `https://react-redux-cart-e04d6-default-rtdb.firebaseio.com/cart.json`,
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!!",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-
-    if(isLoaded){
+  useEffect(() => {
+    if (isLoaded) {
       isLoaded = false;
       return;
     }
-    cartData().catch((error) =>
-      dispatch(
-        uiActions.showNotification({
-          status: "failed",
-          title: "Error",
-          message: "Error in sending cart data",
-        })
-      )
-    );
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
+
   return (
     <Fragment>
     {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
